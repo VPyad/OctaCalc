960362f8c75045d42c9b0b8e7fe546de8c6e148f
Vadim Pyadukhov <vpyadukhov@outlook.com>
2018-03-11 13:14:33 +0300

    add calc layout

--- app/components/calc/calcCtrl.js
+++ app/components/calc/calcCtrl.js
@@ -2,13 +2,16 @@
 
 angular.module('calc', ['ngRoute'])
 
-    .config(['$routeProvider', function($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/calc', {
             templateUrl: 'components/calc/calc.html',
-            controller: 'CalcCtrl'
+            controller: 'CalcCtrl',
+            css: ['bower_components/bootstrap/dist/css/bootstrap.min.css',
+                'bower_components/bootstrap/dist/css/bootstrap-grid.min.css',
+                'components/calc/calc.css']
         });
     }])
 
-    .controller('CalcCtrl', [function() {
+    .controller('CalcCtrl', [function () {
 
     }]);
