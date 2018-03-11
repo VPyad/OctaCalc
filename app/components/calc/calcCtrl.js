da109de5ad004248ed440c48355d2c9bd2396161
Vadim Pyadukhov <vpyadukhov@outlook.com>
2018-03-11 20:34:47 +0300

    add basic on-key-press logic

--- app/components/calc/calcCtrl.js
+++ app/components/calc/calcCtrl.js
@@ -12,6 +12,20 @@ angular.module('calc', ['ngRoute'])
         });
     }])
 
-    .controller('CalcCtrl', [function () {
+    .controller('CalcCtrl', ["$scope", CalcCtrl]);
 
-    }]);
+//const legalInputChars = new RegExp('^(\\d+[\\+\\-\\*\\/]{1})+\\d+$'); //^[-+]?[0-9]+([-+*/]+[-+]?[0-9]+)*$
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7'];
+var operators = ['+', '-', '*', '/'];
+
+function CalcCtrl($scope) {
+    $scope.onCalcInput = function ($event) {
+        var inputChar = String.fromCharCode($event.keyCode);
+        var inputCalcControl = document.getElementById('print');
+        var lastCharOfInput = inputCalcControl.value.slice(-1);
+
+        if (!(digits.includes(inputChar) || (operators.includes(inputChar) && !operators.includes(lastCharOfInput)))) {
+            $event.preventDefault();
+        }
+    }
+}
